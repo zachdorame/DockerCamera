f20d00ebc5b537e45cea75a328ed2ac8ecb6d6c0
Zach Dorame-Barajas <zachdoramebarajas@gmail.com>
2024-06-04 23:06:45 -0700

    sockets and stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,51 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <unistd.h> 
 
-using namespace cv;
+// using namespace cv;
 using namespace std;
 
 int main() {
+    cout << "initializing socket\n";
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    cout << "defining server addrs\n";
+    // define server addrs
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    cout << "binding socket\n";
+    // binding socket
+    bind(serverSocket, (struct sockaddr*)& serverAddress, sizeof(serverAddress));
+
+    cout << "listing for connection\n";
+    // listen
+    listen(serverSocket, 5);
+
+    cout << "binding to connection\n";
+    // accept connection request
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+    /*
+    cout << "receiving data\n";
+    // receiving data
+    char buffer[1024] = { 0 };
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    cout << "Message from client: " << buffer << endl;
+
+    cout << "closing socket\n";
+    // close the socket
+    close(serverSocket);
+
+    return 0;
+    */
+
+
+    /*
     // Open the default camera
     VideoCapture cap(0);
     if (!cap.isOpened()) {
@@ -29,4 +70,5 @@ int main() {
     cap.release();
 
     return 0;
+    */
 }
